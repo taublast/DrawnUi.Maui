9281555d7b4bf92a22f421fe785870df7dc13be2
Nick Kovalsky <nickyboom@mail.ru>
2024-03-21 12:56:13 +0300

    general optimizations

--- src/Engine/Super.cs
+++ src/Engine/Super.cs
@@ -340,7 +340,7 @@ public partial class Super
 
     private static bool _inBackground;
     private static bool initialized;
-    private static bool enableRendering;
+    private static bool enableRendering = true;
 
     public static bool InBackground
     {
