768b7c8283aae4202201389e5f37c9f145f94d87
Nick Kovalsky <nickyboom@mail.ru>
2024-06-12 15:34:09 +0300

    working on shaders

---
+++ src/samples/Sandbox/Views/MainPageShader_Transition.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppoMobi.Maui.Gestures;
+using DrawnUi.Maui.Infrastructure;
+
+namespace Sandbox.Views
+{
+    public partial class MainPageShader_Transition
+    {
+
+        public MainPageShader_Transition()
+        {
+            try
+            {
+                InitializeComponent();
+
+                //avoid setting context BEFORE InitializeComponent, can bug 
+                //having parent BindingContext still null when constructing from xaml
+                BindingContext = new MainPageViewModel();
+
+            }
+            catch (Exception e)
+            {
+                Super.DisplayException(this, e);
+                Console.WriteLine(e);
+            }
+        }
+
+
+
+
+
+    }
+}
