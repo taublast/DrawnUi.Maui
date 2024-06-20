533f8dd0665528615e94adaeeb1e04477f18088d
Nick Kovalsky <nickyboom@mail.ru>
2024-06-20 09:42:21 +0300

    sandbox back

---
+++ src/samples/Sandbox/Views/MainPageDrawnSpans.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Views
+{
+    public partial class MainPageDrawnSpans
+    {
+
+        public MainPageDrawnSpans()
+        {
+            try
+            {
+                InitializeComponent();
+
+
+            }
+            catch (Exception e)
+            {
+                Super.DisplayException(this, e);
+            }
+        }
+
+
+    }
+}
