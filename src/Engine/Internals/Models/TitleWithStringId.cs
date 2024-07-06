0ae12297d88a5c900aa4e52578b1ef1c2340332a
Nick Kovalsky <nickyboom@mail.ru>
2024-07-06 15:45:44 +0300

    Update TitleWithStringId.cs

--- src/Engine/Internals/Models/TitleWithStringId.cs
+++ src/Engine/Internals/Models/TitleWithStringId.cs
@@ -13,31 +13,4 @@ namespace DrawnUi.Maui.Internals
         public string Id { get; set; }
         public string Title { get; set; }
     }
-
-    public class SelectableAction : TitleWithStringId, ISelectableOption
-    {
-        public SelectableAction()
-        {
-            Id = Guid.NewGuid().ToString();
-        }
-        public SelectableAction(string title, Action action)
-        {
-            Id = Guid.NewGuid().ToString();
-            Title = title;
-            Action = action;
-        }
-
-        public SelectableAction(string id, string title, Action action)
-        {
-            Id = id;
-            Title = title;
-            Action = action;
-        }
-
-
-        public Action Action { get; set; }
-        public bool Selected { get; set; }
-
-        public bool IsReadOnly { get; } = false;
-    }
 }
