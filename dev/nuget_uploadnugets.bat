a5e1ccd5fd8f88282c9bd112e5e7a41ead7e6185
Nick Kovalsky <nickyboom@mail.ru>
2024-11-03 00:38:22 +0300

    1.2.4.7

--- dev/nuget_uploadnugets.bat
+++ dev/nuget_uploadnugets.bat
@@ -1,9 +1,7 @@
 @echo off
-
-set "file_masks=DrawnUi.Maui*.1.2.4.6*.nupkg AppoMobi.Maui.DrawnUi.1.2.4.6*.*nupkg"
-set "source_dir=E:\Nugets"
-
 setlocal enabledelayedexpansion
+
+REM Check if API key is provided as a parameter
 if "%~1"=="" (
     REM Ask for API key
     set /p "APIKEY=Please enter your NUGET.ORG API key: "
@@ -11,17 +9,26 @@ if "%~1"=="" (
     set "APIKEY=%~1"
 )
 
-REM Loop over each file mask
-for %%m in (%file_masks%) do (
-    echo Processing files matching %%m
-    REM Loop through each package file in the source directory matching current mask
-    for %%f in ("%source_dir%\%%m") do (
-        echo Uploading %%f to NUGET.ORG with API key.
-        nuget push %%f -Source https://api.nuget.org/v3/index.json -ApiKey %APIKEY%
+REM Define the source directory for the packages
+set "source_dir=E:\Nugets"
+
+REM Define the list of file masks for the packages
+set "mask[1]=DrawnUi.Maui*.1.2.4.7*.nupkg"
+set "mask[2]=AppoMobi.Maui.DrawnUi.1.2.4.7*.*nupkg"
+set "mask_count=2"
+
+REM Loop through each file mask
+for /L %%i in (1,1,%mask_count%) do (
+    set "file_mask=!mask[%%i]!"
+    echo Using file mask "!file_mask!"
+    REM Loop through each package file matching the mask in the source directory
+    for %%f in ("!source_dir!\!file_mask!") do (
+        echo Uploading "%%f" to NUGET.ORG with API key.
+        nuget push "%%f" -Source https://api.nuget.org/v3/index.json -ApiKey %APIKEY%
         if errorlevel 1 (
-            echo An error occurred while uploading %%f.
+            echo An error occurred while uploading "%%f".
         ) else (
-            echo %%f uploaded successfully.
+            echo "%%f" uploaded successfully.
         )
     )
 )
