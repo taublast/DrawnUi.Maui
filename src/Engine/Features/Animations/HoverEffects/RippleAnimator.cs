33e6e990e554377c73d436b7a562e0a37768d5df
Nick Kovalsky <nickyboom@mail.ru>
2024-12-06 10:15:23 +0300

    TextTransform prop for SkiaLabel

---
+++ src/Engine/Features/Animations/HoverEffects/RippleAnimator.cs
@@ -0,0 +1,88 @@
+﻿namespace DrawnUi.Maui.Draw;
+
+public class RippleAnimator : RenderingAnimator
+{
+    public RippleAnimator(IDrawnBase control) : base(control)
+    {
+        IsPostAnimator = true;
+        Speed = 500;//250;
+        mMinValue = 0;
+        mMaxValue = 1;
+        Color = SKColor.Parse("#FFFFFF");
+        Easing = Easing.CubicIn;
+    }
+
+    protected static long count;
+
+    public SKColor Color { get; set; }
+
+    public static double DiameterDefault = 300.0;
+
+    public static double OpacityDefault = 0.20;
+
+    /// <summary>
+    /// In pts relative to control X,Y. These are coords inside the control and not inside the canvas. 
+    /// </summary>
+    public double X { get; set; }
+
+    /// <summary>
+    /// In pts relative to control X,Y. These are coords inside the control and not inside the canvas. 
+    /// </summary>
+    public double Y { get; set; }
+
+    public double Diameter { get; set; }
+
+    public double Opacity { get; set; }
+
+    public override void Dispose()
+    {
+        if (Parent != null)
+        {
+            Parent.DisposeObject(Paint);
+        }
+
+        base.Dispose();
+    }
+
+    protected SKPaint Paint;
+
+    protected override bool OnRendering(IDrawnBase control, SkiaDrawingContext context, double scale)
+    {
+        if (IsRunning && control != null && !control.IsDisposed && !control.IsDisposing)
+        {
+            var touchOffset = new SKPoint((float)(X * scale), (float)(Y * scale));
+            var selfDrawingLocation = GetSelfDrawingLocation(control);
+
+            DrawWithClipping(context, control, selfDrawingLocation, () =>
+            {
+                Paint ??= new SKPaint();
+                Paint.Style = SKPaintStyle.Fill;
+                Paint.Color = Color.WithAlpha((byte)(Opacity * 255));
+
+                touchOffset.Offset(selfDrawingLocation);
+                context.Canvas.DrawCircle(touchOffset.X, touchOffset.Y, (float)(Diameter * scale), Paint);
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected override double TransformReportedValue(long deltaT)
+    {
+
+        var progress = base.TransformReportedValue(deltaT);
+
+        var opacityProgress = progress * 1.15;
+        if (opacityProgress <= 1)
+        {
+            Opacity = OpacityDefault - OpacityDefault * opacityProgress;
+        }
+
+        Diameter = DiameterDefault * progress;
+
+        return progress;
+    }
+
+}
---
+++ src/samples/Sandbox/MainPageCodeDev.cs
@@ -0,0 +1,96 @@
+﻿using Sandbox.Views;
+using Canvas = DrawnUi.Maui.Views.Canvas;
+using TextTransform = DrawnUi.Maui.Draw.TextTransform;
+
+namespace Sandbox
+{
+
+
+    public class MainPageCodeDev : BasePage, IDisposable
+    {
+        Canvas Canvas;
+
+        public void Dispose()
+        {
+            this.Content = null;
+            Canvas?.Dispose();
+        }
+
+
+
+
+        public MainPageCodeDev()
+        {
+#if DEBUG
+            HotReloadService.UpdateApplicationEvent += ReloadUI;
+#endif
+            Build();
+        }
+
+        private int _reloads;
+
+        void Build()
+        {
+            Canvas?.Dispose();
+
+            Canvas = new Canvas()
+            {
+                Gestures = GesturesMode.Enabled,
+                HardwareAcceleration = HardwareAccelerationMode.Disabled,
+
+                VerticalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.Fill,
+                BackgroundColor = Colors.LightGray,
+
+                Content = new SkiaLayout()
+                {
+                    VerticalOptions = LayoutOptions.Fill,
+                    HorizontalOptions = LayoutOptions.Fill,
+                    Children = new List<SkiaControl>()
+                    {
+                        new SkiaLayout()
+                        {
+                            Children = new List<SkiaControl>()
+                            {
+                                new SkiaShape()
+                                {
+                                    HorizontalOptions = LayoutOptions.Fill,
+                                    HeightRequest = 128,
+                                    StrokeColor = Colors.Black,
+                                    StrokeWidth = 1,
+                                    Margin = 8,
+                                    CornerRadius = 10,
+                                    Padding = 10,
+                                    BackgroundColor = Colors.White,
+                                    Content = new SkiaMarkdownLabel()
+                                    {
+                                        Text = "hello ppl how are you\r\nNew line comes here\r\nAnd another one too",
+                                        TextTransform = TextTransform.Titlecase,
+                                        MaxLines = 2
+                                    }
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+
+            };
+
+            _reloads++;
+
+            this.Content = Canvas;
+        }
+
+        private void ReloadUI(Type[] obj)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Build();
+            });
+        }
+
+    }
+}
