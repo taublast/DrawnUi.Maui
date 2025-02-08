930ab8d0dbb1ddbb6d272d2be62451b41a770d51
Nick Kovalsky <nickyboom@mail.ru>
2025-02-08 12:31:12 +0300

    Update PointedDirectionType.cs

--- src/Engine/Shared/Draw/Internals/Enums/PointedDirectionType.cs
+++ src/Engine/Shared/Draw/Internals/Enums/PointedDirectionType.cs
@@ -15,7 +15,13 @@ public enum LinearDirectionType
 {
     None,
 
+    /// <summary>
+    /// Or down
+    /// </summary>
     Forward,
 
+    /// <summary>
+    /// Or up
+    /// </summary>
     Backward,
 }
