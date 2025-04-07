a4a26046a2026bd9c1f77fe1a6ef97c3c2bc564f
Nick Kovalsky <nickyboom@mail.ru>
2025-04-07 12:01:06 +0300

    ios safe area maui fix

--- src/Engine/Maui/Platforms/iOS/ConfigureHandlers.iOS.cs
+++ src/Engine/Maui/Platforms/iOS/ConfigureHandlers.iOS.cs
@@ -7,24 +7,28 @@ namespace DrawnUi.Draw
     {
         public static void ConfigureHandlers(IMauiHandlersCollection handlers)
         {
-            if (StartupSettings.MobileIsFullscreen)
+            bool useFullScreen = false;
+            if (StartupSettings != null && StartupSettings.MobileIsFullscreen)
             {
-                PageHandler.Mapper.AppendToMapping("Custom", (h, v) =>
-                {
-                    if (v is Microsoft.Maui.Controls.Page page)
-                    {
-                        Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(page, false);
-                    }
-                });
-
-                LayoutHandler.Mapper.AppendToMapping("Custom", (h, v) =>
-                {
-                    if (v is Layout layout)
-                    {
-                        layout.IgnoreSafeArea = true;
-                    }
-                });
+                useFullScreen = StartupSettings.MobileIsFullscreen;
             }
+            var useSafeArea = !useFullScreen;
+
+            PageHandler.Mapper.AppendToMapping("Custom", (h, v) =>
+                 {
+                     if (v is Microsoft.Maui.Controls.Page page)
+                     {
+                         Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(page, useSafeArea);
+                     }
+                 });
+
+            LayoutHandler.Mapper.AppendToMapping("Custom", (h, v) =>
+            {
+                if (v is Layout layout)
+                {
+                    layout.IgnoreSafeArea = useFullScreen;
+                }
+            });
 
             handlers.AddHandler(typeof(MauiEntry), typeof(MauiEntryHandler));
             handlers.AddHandler(typeof(MauiEditor), typeof(MauiEditorHandler));
