ba7d59ac47a75588f3cac0864a2c8e2ac5a40cd4
Nick Kovalsky <taublast@gmail.com>
2025-06-21 07:36:07 +0300

    refactored

---
+++ src/Maui/DrawnUi/Platforms/Windows/Views/SKAutoCanvasRestoreFixed.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawnUi.Views
+{
+    public class SKAutoCanvasRestoreFixed : IDisposable
+    {
+        private SKCanvas canvas;
+        private readonly int saveCount;
+
+        public SKAutoCanvasRestoreFixed(SKCanvas canvas)
+            : this(canvas, true)
+        {
+        }
+
+        public SKAutoCanvasRestoreFixed(SKCanvas canvas, bool doSave)
+        {
+            this.canvas = canvas;
+            this.saveCount = 0;
+
+            if (canvas != null)
+            {
+                saveCount = canvas.SaveCount;
+                if (doSave)
+                {
+                    canvas.Save();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (canvas != null && canvas.Handle != IntPtr.Zero)
+            {
+                Restore();
+            }
+        }
+
+        /// <summary>
+        /// Perform the restore now, instead of waiting for the Dispose.
+        /// Will only do this once.
+        /// </summary>
+        public void Restore()
+        {
+            if (canvas != null)
+            {
+                canvas.RestoreToCount(saveCount);
+                canvas = null;
+            }
+        }
+    }
+}
--- src/Maui/DrawnUi/Platforms/Windows/Views/SKSwapChainPanelRetained.cs
+++ src/Maui/DrawnUi/Platforms/Windows/Views/SKSwapChainPanelRetained.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp.Views.Windows;
+﻿using System.Collections.Concurrent;
+using SkiaSharp.Views.Windows;
 
 namespace DrawnUi.Views
 {
@@ -145,55 +146,6 @@ namespace DrawnUi.Views
                 _needsFullRedraw = true;
             }
 
-        }
-    }
-
-
-
-    public class SKAutoCanvasRestoreFixed : IDisposable
-    {
-        private SKCanvas canvas;
-        private readonly int saveCount;
-
-        public SKAutoCanvasRestoreFixed(SKCanvas canvas)
-            : this(canvas, true)
-        {
-        }
-
-        public SKAutoCanvasRestoreFixed(SKCanvas canvas, bool doSave)
-        {
-            this.canvas = canvas;
-            this.saveCount = 0;
-
-            if (canvas != null)
-            {
-                saveCount = canvas.SaveCount;
-                if (doSave)
-                {
-                    canvas.Save();
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            if (canvas != null && canvas.Handle != IntPtr.Zero)
-            {
-                Restore();
-            }
-        }
-
-        /// <summary>
-        /// Perform the restore now, instead of waiting for the Dispose.
-        /// Will only do this once.
-        /// </summary>
-        public void Restore()
-        {
-            if (canvas != null)
-            {
-                canvas.RestoreToCount(saveCount);
-                canvas = null;
-            }
         }
     }
 
