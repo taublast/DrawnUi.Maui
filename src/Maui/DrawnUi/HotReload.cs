3b8e05010a9ecc9784a5196b52fbf4d7651c6852
Nick Kovalsky <taublast@gmail.com>
2025-06-24 15:58:02 +0300

    Update HotReload.cs

--- src/Maui/DrawnUi/HotReload.cs
+++ src/Maui/DrawnUi/HotReload.cs
@@ -15,7 +15,7 @@ namespace DrawnUi
 
         private static RestartingTimer UpdateOnTimerOnly = new (DelayMs, () =>
         {
-            Trace.WriteLine("[HOTRELOAD] Updating Application");
+            Trace.WriteLine("[HOTRELOAD] Updating Application =>");
             UpdateApplicationEvent?.Invoke(null);
         });
 
