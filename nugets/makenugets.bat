93c7866b7fee8f74bf7cad00cb75801f2dfb4ea1
Nick Kovalsky <taublast@gmail.com>
2025-07-01 20:30:32 +0300

    1.5.1.7-pre

--- nugets/makenugets.bat
+++ nugets/makenugets.bat
@@ -3,7 +3,7 @@
 
 dotnet pack ..\src\Maui\DrawnUi\DrawnUi.Maui.csproj
 dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.Camera\DrawnUi.Maui.Camera.csproj
-dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.Game\DrawnUi.Maui.Gamera.csproj
+dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.Game\DrawnUi.Maui.Game.csproj
 dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.MapsUi\DrawnUi.Maui.MapsUi.csproj
 dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.Rive\DrawnUi.Maui.Rive.csproj
 dotnet pack ..\src\Maui\Addons\DrawnUi.Maui.Camera\DrawnUi.Maui.Camera.csproj
